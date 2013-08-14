f0161349ea53bff914d880433a093fb8b031792c
Tim Roberts <tim@zumointernet.com>
2013-08-14 17:26:23 +0200

    Initial import

---
+++ send.js
@@ -0,0 +1,40 @@
+// load aws sdk
+var aws = require('aws-sdk');
+
+// load aws config
+aws.config.loadFromPath('config.json');
+
+// load AWS SES
+var ses = new aws.SES({apiVersion: '2010-12-01'});
+
+// send to list
+var to = ['email@example.com']
+
+// this must relate to a verified SES account
+var from = 'emailc@example.com'
+
+
+// this sends the email
+// @todo - add HTML version
+ses.sendEmail( 
+	{ 
+		Source: from, 
+		Destination: { ToAddresses: to },
+		Message: {
+			Subject: {
+				Data: 'A Message To You Rudy'
+			},
+			Body: {
+				Text: {
+						Data: 'Stop your messing around',
+				}
+			}
+		}
+	}
+, function(err, data) {
+	if(err) throw err
+	console.log('Email sent:');
+	console.log(data);
+});
+
+
